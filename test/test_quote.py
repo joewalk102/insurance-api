8230ddbffe859748e54120155c9d8d409b59f37a
Joe Walker <joewalk102@gmail.com>
2022-05-28 11:37:29 -0700

    Add tests to check for edit and delete

--- test/test_quote.py
+++ test/test_quote.py
@@ -9,12 +9,9 @@ class TestQuoteCreate(APITestCase):
             "date_effective": "2022-01-01T00:00:00.000",
             "date_previous_canceled": "2022-01-01",
             "is_owned": False,
-            "address": {
-                "state": "WA",
-                "zipcode": "99999"
-            }
+            "address": {"state": "WA", "zipcode": "99999"},
         }
-        response = self.client.post('/quotes/', data, format='json')
+        response = self.client.post("/quotes/", data, format="json")
 
         obj = Quote.objects.first()
 
@@ -23,10 +20,9 @@ class TestQuoteCreate(APITestCase):
             "date_effective": "2022-01-01T00:00:00Z",
             "date_previous_canceled": "2022-01-01",
             "is_owned": False,
-            "address": {
-                "zipcode": 99999,
-                "state": "WA"
-            }
+            "cost_monthly": 13.986,
+            "cost_biannually": 83.916,
+            "address": {"zipcode": 99999, "state": "WA"},
         }
 
     def test_dedupe_address(self):
@@ -34,15 +30,12 @@ class TestQuoteCreate(APITestCase):
             "date_effective": "2022-01-01T00:00:00.000",
             "date_previous_canceled": "2022-01-01",
             "is_owned": False,
-            "address": {
-                "state": "WA",
-                "zipcode": "99999"
-            }
+            "address": {"state": "WA", "zipcode": "99999"},
         }
 
         # we should have two quotes but only one address
-        self.client.post('/quotes/', data, format='json')
-        self.client.post('/quotes/', data, format='json')
+        self.client.post("/quotes/", data, format="json")
+        self.client.post("/quotes/", data, format="json")
 
         assert Quote.objects.count() == 2
         assert Address.objects.count() == 1
@@ -54,25 +47,21 @@ class TestQuoteGet(APITestCase):
             "date_effective": "2022-01-01T00:00:00.000",
             "date_previous_canceled": "2022-01-01",
             "is_owned": False,
-            "address": {
-                "state": "WA",
-                "zipcode": "99999"
-            }
+            "address": {"state": "WA", "zipcode": "99999"},
         }
-        response = self.client.post('/quotes/', data, format='json')
+        response = self.client.post("/quotes/", data, format="json")
         qid = response.json()["qid"]
 
-        response = self.client.get(f"/quotes/{qid}/", format='json')
+        response = self.client.get(f"/quotes/{qid}/", format="json")
 
         assert response.json() == {
             "qid": qid,
             "date_effective": "2022-01-01T00:00:00Z",
             "date_previous_canceled": "2022-01-01",
             "is_owned": False,
-            "address": {
-                "zipcode": 99999,
-                "state": "WA"
-            }
+            "cost_monthly": 13.986,
+            "cost_biannually": 83.916,
+            "address": {"zipcode": 99999, "state": "WA"},
         }
 
     def test_get_all(self):
@@ -80,17 +69,72 @@ class TestQuoteGet(APITestCase):
             "date_effective": "2022-01-01T00:00:00.000",
             "date_previous_canceled": "2022-01-01",
             "is_owned": False,
-            "address": {
-                "state": "WA",
-                "zipcode": "99999"
-            }
+            "address": {"state": "WA", "zipcode": "99999"},
         }
-        [self.client.post('/quotes/', data, format='json') for _ in range(5)]
+        # create 5 quotes
+        [self.client.post("/quotes/", data, format="json") for _ in range(5)]
 
-        response = self.client.get(f"/quotes/", format='json').json()
+        response = self.client.get(f"/quotes/", format="json").json()
 
         assert response["count"] == 5
         assert "next" in response
         assert "previous" in response
         assert "results" in response
         assert len(response["results"]) == 5
+
+
+class TestQuoteEdit(APITestCase):
+    def test_edit(self):
+        initial_data = {
+            "date_effective": "2022-01-01T00:00:00.000",
+            "date_previous_canceled": "2022-01-01",
+            "is_owned": False,
+            "address": {"state": "WA", "zipcode": "99999"},
+        }
+        self.client.post("/quotes/", initial_data, format="json")
+        new_data = {
+            "date_effective": "2022-01-01T00:00:00.000",
+            "date_previous_canceled": None,
+            "is_owned": True,
+            "address": {"state": "WA", "zipcode": "99999"},
+        }
+        response = self.client.post("/quotes/", new_data, format="json")
+
+        obj = Quote.objects.first()
+
+        assert response.json() == {
+            "qid": obj.qid,
+            "date_effective": "2022-01-01T00:00:00Z",
+            "date_previous_canceled": None,
+            "is_owned": True,
+            "address": {"zipcode": 99999, "state": "WA"},
+            "cost_monthly": 9.4905,
+            "cost_biannually": 56.943,
+        }
+
+
+class TestQuoteDelete(APITestCase):
+    def test_delete(self):
+        data = {
+            "date_effective": "2022-01-01T00:00:00.000",
+            "date_previous_canceled": "2022-01-01",
+            "is_owned": False,
+            "address": {"state": "WA", "zipcode": "99999"},
+        }
+        response = self.client.post("/quotes/", data, format="json")
+
+        obj = Quote.objects.first()
+
+        assert response.json() == {
+            "qid": obj.qid,
+            "date_effective": "2022-01-01T00:00:00Z",
+            "date_previous_canceled": "2022-01-01",
+            "is_owned": False,
+            "cost_monthly": 13.986,
+            "cost_biannually": 83.916,
+            "address": {"zipcode": 99999, "state": "WA"},
+        }
+
+        response = self.client.delete(f"/quotes/{obj.qid}/")
+        assert response.status_code == 204
+        assert Quote.objects.first() is None
