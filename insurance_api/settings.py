2e51660a3aec5835bcf8f30406208ca4165cf9f6
Joe Walker <joewalk102@gmail.com>
2022-05-28 12:07:11 -0700

    Use random secret key on start.

--- insurance_api/settings.py
+++ insurance_api/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import random
+import string
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i(kgkt1k+f-xzjsi%sxi(ls*$-9i-!v%j71+6d%3m)-7_=k=)3"
+SECRET_KEY = "".join([random.choice(string.printable) for _ in range(66)])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
